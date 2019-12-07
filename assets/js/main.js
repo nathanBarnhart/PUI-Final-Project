5471f3309fad2b94b9134c7e63e7010ef8d89c5c
Nathan Barnhart <nbarnhart@protonmail.com>
2019-12-06 23:06:11 -0500

    javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,75 @@
-//initial setup
-const container = document.querySelector('#scene-container');
 
-//the scene
-const scene = new THREE.Scene();
+//declaring global variables
+let container;
+let camera;
+let renderer;
+let scene;
+let mesh;
 
-scene.background = new THREE.Color('skyblue');
+function init() {
 
-//the camera (1 unit = 1 meter)
-const fov = 35; //range: 1-79; 40-60 for TV; ~90 for computer screen
-const aspect = container.clientWidth / container.clientHeight;
-const near = 0.1; //near clipping plane
-const far = 100; //far clipping plane; keep as low as possible for high FPS
+    //reference to the DOM container element for scene
+    container = document.querySelector('#scene-container');
 
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    //creating scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x8FBCD4);
 
-camera.position.set(0, 0, 10); //origin: 0,0,0; x (-side to +side), y (-down/+up), z (-far/+near)
+    //options for perspective camera
+    const fov = 35; //range: 1-79; 40-60 for TV; ~90 for computer screen
+    const aspect = container.clientWidth / container.clientHeight;
+    const near = 0.1;
+    const far = 100; //keep as low as possible for high FPS
 
-//visible objects
-const geometry = new THREE.BoxBufferGeometry(2, 2, 2); //'buffer geometry' is loads faster than 'geometry'
-const material = new THREE.MeshBasicMaterial();
-const mesh = new THREE.Mesh(geometry, material);
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 0, 10); //origin: 0,0,0; x (-side to +side), y (-down/+up), z (-far/+near)
 
-scene.add(mesh);
+    //creating geometry
+    const geometry = new THREE.BoxBufferGeometry(2, 2, 2); //'buffer geometry' is loads faster than 'geometry'
 
+    //creating material with a given color
+    const material = new THREE.MeshStandardMaterial({ color: 0x800080 });
 
-//the renderer
-const renderer = new THREE.WebGLRenderer();
+    //creating mesh
+    mesh = new THREE.Mesh(geometry, material);
 
-renderer.setSize(container.clientWidth, container.clientHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
+    //adding mesh to scene
+    scene.add(mesh);
 
-container.appendChild(renderer.domElement);
+    //creating a directional light
+    const light = new THREE.DirectionalLight(0xffffff, 5.0);
 
-//render the scene
-renderer.render(scene, camera);
+    //positioning light
+    light.position.set(10, 10, 10);
 
+    //adding light to scene
+    scene.add(light);
+
+    //creating renderer (WebGLRenderer); set width/height
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    //automatically creates <canvas> element on webpage when loaded by browser
+    container.appendChild(renderer.domElement);
+}
+
+function animate() {
+    //calling animate recursively
+    requestAnimationFrame(animate);
+
+    //increases the rotation of mesh each frame
+    mesh.rotation.z += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+
+    //rendering scene; one still image / animation frame
+    renderer.render(scene, camera);
+
+}
+
+//function to set up everything
+init();
+
+//animating function to render the scene
+animate();
