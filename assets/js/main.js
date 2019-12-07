09e1af974b2e8fd9d63cb13e31ab2f82dcb981c6
Nathan Barnhart <nbarnhart@protonmail.com>
2019-12-07 02:43:44 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@ function createControls() {
 
 function createLights() {
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
     scene.add(ambientLight);
 
     const mainLight = new THREE.DirectionalLight(0xffffff, 1);
